6df5b7fbb1771d3c4996c623a6a5ba6613a27c66
Emayoung <emayoung95@gmail.com>
2020-05-06 16:41:17 +0100

    Fixed recyclerview truncated when using data binding

--- app/src/main/java/com/threedee/nature/home/FarmersAdapter.kt
+++ app/src/main/java/com/threedee/nature/home/FarmersAdapter.kt
@@ -1,36 +1,37 @@
 package com.threedee.nature.home
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.threedee.domain.model.Farm
-import com.threedee.nature.databinding.DashboardInfoBinding
+import com.threedee.nature.R
+import com.threedee.nature.util.inflate
+import kotlinx.android.synthetic.main.dashboard_info.view.*
 
-class FarmersAdapter(private var items: ArrayList<Farm>,
-    private val itemClick: (farm: Farm) -> Unit) : RecyclerView.Adapter<FarmersAdapter.ViewHolder>() {
+class FarmersAdapter(
+    private var items: ArrayList<Farm>,
+    private val itemClick: (farm: Farm) -> Unit
+) : RecyclerView.Adapter<FarmersAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = DashboardInfoBinding.inflate(inflater)
-        return ViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder{
+        return ViewHolder(parent)
     }
 
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(items[position])
 
-    fun setData(farms: List<Farm>){
+    fun setData(farms: List<Farm>) {
         items.clear()
         items.addAll(farms)
         notifyDataSetChanged()
     }
-    inner class ViewHolder(val binding: DashboardInfoBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    inner class ViewHolder(parent: ViewGroup) :
+        RecyclerView.ViewHolder(parent.inflate(R.layout.dashboard_info)) {
         fun bind(item: Farm) {
-            with(binding) {
-                //TODO: Add GlideApp and details and date format
-                binding.farmerNameTextView.text = "Christiana Olaoye"
-                binding.root.setOnClickListener { itemClick.invoke(item) }
-            }
+            //TODO: Add GlideApp and details and date format
+            itemView.farmerNameTextView.text = "Christiana Olaoye"
+            itemView.setOnClickListener { itemClick.invoke(item) }
         }
     }
 }
